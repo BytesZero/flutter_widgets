206f60bfffe8a76e08be1a3b6425705f8bdc0400
Zero <1300326388@qq.com>
2021-06-02 02:12:28 +0800

    1、完善 AnimatedContainer 示例

--- lib/widgets/01_animated_container.dart
+++ lib/widgets/01_animated_container.dart
@@ -14,8 +14,11 @@ class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
   // 定义属性
   double width = 200;
   double height = 100;
+  double padding = 10;
   Color color = Colors.blueAccent;
   Alignment alignment = Alignment.center;
+  double borderWidth = 4;
+  double radius = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +27,54 @@ class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
         title: Text('AnimatedContainer'),
       ),
       body: Center(
-        child: AnimatedContainer(
-          width: width,
-          height: height,
-          color: color,
-          duration: const Duration(milliseconds: 500),
-          alignment: alignment,
-          child: Text(
-            'ZeroFlutter',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Container(
+            //   width: width,
+            //   height: height,
+            //   padding: EdgeInsets.all(padding),
+            //   color: color,
+            //   alignment: alignment,
+            //   child: Text(
+            //     'ZeroFlutter',
+            //     style: TextStyle(
+            //       color: Colors.white,
+            //     ),
+            //   ),
+            // ),
+            // SizedBox(width: 30),
+            AnimatedContainer(
+              width: width,
+              height: height,
+              padding: EdgeInsets.all(padding),
+              margin: EdgeInsets.all(padding),
+              // color: color,
+              alignment: alignment,
+              duration: const Duration(milliseconds: 500),
+              decoration: BoxDecoration(
+                color: color,
+                border: Border.all(
+                  width: borderWidth,
+                  color: color.withOpacity(0.4),
+                ),
+                borderRadius: BorderRadius.circular(radius),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: radius,
+                    offset: Offset.zero,
+                  )
+                ],
+              ),
+              child: Text(
+                'ZeroFlutter',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -50,16 +89,23 @@ class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
   void play() async {
     var width = Random.secure().nextInt(150);
     var height = Random.secure().nextInt(100);
+    var padding = Random.secure().nextInt(30);
     var a = Random.secure().nextInt(256);
     var r = Random.secure().nextInt(256);
     var g = Random.secure().nextInt(256);
     var b = Random.secure().nextInt(256);
     var x = Random.secure().nextInt(3);
     var y = Random.secure().nextInt(3);
+    var borderWidth = Random.secure().nextInt(10);
+    var radius = Random.secure().nextInt(30);
+
     this.width = width.toDouble() + 200;
     this.height = height.toDouble() + 100;
+    this.padding = padding.toDouble();
     this.color = Color.fromARGB(255, r, g, b);
     this.alignment = Alignment(x - 1, y - 1);
+    this.borderWidth = borderWidth.toDouble();
+    this.radius = radius.toDouble();
     setState(() {});
   }
 }
