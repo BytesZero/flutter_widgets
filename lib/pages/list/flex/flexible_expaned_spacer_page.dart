4b7ef865ac8b80f2fc2495fee54393f3ecce71c2
Zero <1300326388@qq.com>
2021-06-12 13:12:45 +0800

    1、更新代码

--- lib/pages/list/flex/flexible_expaned_spacer_page.dart
+++ lib/pages/list/flex/flexible_expaned_spacer_page.dart
@@ -13,91 +13,151 @@ class _FlexiblePageState extends State<FlexiblePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Row、Column、Flex 详解 - ZeroFlutter'),
+        title: Text('Row、Expanded、Spacer - ZeroFlutter'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text('Flexible'),
-              ),
-            ),
-            BgContainer(
-              child: Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: getItem(1),
+      body: Center(
+        child: SingleChildScrollView(
+          child: BgContainer(
+            child: Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text('Flexible'),
                   ),
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.tight,
-                    child: getItem(2),
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.loose,
+                      child: getItem(1),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      fit: FlexFit.loose,
+                      child: getItem(1),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      fit: FlexFit.loose,
+                      child: getItem(1),
+                    ),
+                  ],
+                ),
+                // Row(
+                //   children: [
+                //     Flexible(
+                //       flex: 1,
+                //       fit: FlexFit.loose,
+                //       child: getItem(1, null, 60),
+                //     ),
+                //     Flexible(
+                //       flex: 2,
+                //       fit: FlexFit.loose,
+                //       child: getItem(1, null, 60),
+                //     ),
+                //     Flexible(
+                //       flex: 3,
+                //       fit: FlexFit.loose,
+                //       child: getItem(1, null, 60),
+                //     ),
+                //   ],
+                // ),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: getItem(1),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      fit: FlexFit.tight,
+                      child: getItem(2),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      fit: FlexFit.tight,
+                      child: getItem(3),
+                    ),
+                  ],
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text('Expanded'),
                   ),
-                  Flexible(
-                    flex: 3,
-                    fit: FlexFit.tight,
-                    child: getItem(3),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: getItem(1),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: getItem(2),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: getItem(3),
+                    ),
+                  ],
+                ),
+                // Row(
+                //   children: [
+                //     getItem(1),
+                //     Expanded(
+                //       child: Text(
+                //         'ZeroFlutter 聊 Expanded，ZeroFlutter 聊 ExpandedZeroFlutter 聊 ExpandedZeroFlutter 聊 ExpandedZeroFlutter 聊 ExpandedZeroFlutter 聊 Expanded',
+                //         overflow: TextOverflow.ellipsis,
+                //         maxLines: 2,
+                //       ),
+                //     )
+                //     // Expanded(
+                //     //   flex: 1,
+                //     //   child: getItem(1),
+                //     // ),
+                //     // Expanded(
+                //     //   flex: 2,
+                //     //   child: getItem(2),
+                //     // ),
+                //     // Expanded(
+                //     //   flex: 3,
+                //     //   child: getItem(3),
+                //     // ),
+                //   ],
+                // ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text('Spacer'),
                   ),
-                ],
-              ),
+                ),
+                Row(
+                  children: [
+                    getItem(1),
+                    Spacer(),
+                    getItem(2),
+                    Spacer(flex: 2),
+                    getItem(3)
+                  ],
+                ),
+              ],
             ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text('Expanded'),
-              ),
-            ),
-            BgContainer(
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: getItem(1),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: getItem(2),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: getItem(3),
-                  ),
-                ],
-              ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text('Spacer'),
-              ),
-            ),
-            BgContainer(
-              child: Row(
-                children: [
-                  getItem(1),
-                  Spacer(flex: 1),
-                  getItem(2),
-                  Spacer(flex: 2),
-                  getItem(3)
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 
   /// 获取子项目
-  Widget getItem(int index) {
+  Widget getItem(int index, [double? width = 60, double? height = 60]) {
     return Container(
       // 宽高设置 60
-      width: 60,
-      height: 60,
+      width: width,
+      height: height,
       // 设置背景色
       color: Colors.orange.shade200,
       // 设置间隙
