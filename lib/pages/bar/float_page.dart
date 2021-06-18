1bb75924114428b5830e72aedc8461a9122a050d
Zero <1300326388@qq.com>
2021-06-18 23:20:35 +0800

    1、更新 FloatingActionButton

--- lib/pages/bar/float_page.dart
+++ lib/pages/bar/float_page.dart
@@ -9,36 +9,112 @@ class FloatActionBarPage extends StatefulWidget {
 }
 
 class _FloatActionBarPageState extends State<FloatActionBarPage> {
+  int currentIndex = 0;
+  List<FloatingActionButtonLocation> locationList = [
+    FloatingActionButtonLocation.startFloat,
+    FloatingActionButtonLocation.centerFloat,
+    FloatingActionButtonLocation.endFloat,
+    FloatingActionButtonLocation.startDocked,
+    FloatingActionButtonLocation.centerDocked,
+    // FloatingActionButtonLocation.miniCenterDocked,
+    FloatingActionButtonLocation.endDocked,
+    FloatingActionButtonLocation.startTop,
+    FloatingActionButtonLocation.centerTop,
+    FloatingActionButtonLocation.endTop,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FloatActionBar - ZeroFlutter'),
+        title: Text('FloatingActionButton - ZeroFlutter'),
       ),
       body: Center(
         child: Text('ZeroFlutter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
-        clipBehavior: Clip.antiAlias,
+        onPressed: () {
+          currentIndex += 1;
+          setState(() {});
+        },
+        tooltip: '${locationList[currentIndex % locationList.length]}',
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButton: SizedBox(
+      //   width: 80,
+      //   height: 80,
+      //   child: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      // backgroundColor: Colors.orange,
+      // foregroundColor: Colors.orange,
+      // splashColor: Colors.orange,
+      // elevation: 6,
+      // highlightElevation: 12,
+      // mini: true,
+      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      // shape: ContinuousRectangleBorder(
+      //   side: BorderSide(
+      //     width: 4,
+      //     color: Colors.orange,
+      //   ),
+      //   borderRadius: BorderRadius.circular(12),
+      // ),
+      // shape: BeveledRectangleBorder(
+      //   side: BorderSide(
+      //     width: 4,
+      //     color: Colors.orange,
+      //   ),
+      //   borderRadius: BorderRadius.circular(12),
+      // ),
+      // onPressed: () {
+      // currentIndex += 1;
+      // setState(() {});
+      // },
+      // tooltip: '${locationList[currentIndex % locationList.length]}',
+      //   ),
+      // ),
+      // floatingActionButton: Column(
+      //   mainAxisSize: MainAxisSize.min,
+      //   children: [
+      //     FloatingActionButton(
+      //       child: Icon(Icons.ac_unit_rounded),
+      //       backgroundColor: Colors.orange,
+      //       heroTag: 'tag1',
+      //       onPressed: () {},
+      //     ),
+      //     SizedBox(height: 10),
+      //     FloatingActionButton(
+      //       child: Icon(Icons.adb_sharp),
+      //       backgroundColor: Colors.green,
+      //       heroTag: 'tag2',
+      //       onPressed: () {},
+      //     ),
+      //     SizedBox(height: 10),
+      //     FloatingActionButton(
+      //       child: Icon(Icons.more_horiz_outlined),
+      //       heroTag: 'tag3',
+      //       onPressed: () {},
+      //     ),
+      //   ],
+      // ),
+      floatingActionButtonLocation:
+          locationList[currentIndex % locationList.length],
       bottomNavigationBar: BottomAppBar(
         /// 圆形缺口矩形
         shape: CircularNotchedRectangle(),
 
         /// 自动缺口形状
         // shape: AutomaticNotchedShape(
-        //   // 斜角矩形
-        //   // BeveledRectangleBorder(
-        //   //   borderRadius: BorderRadius.circular(20),
-        //   // ),
-        //   // 圆角矩形
-        //   RoundedRectangleBorder(
-        //     borderRadius: BorderRadius.circular(40),
-        //   ),
+        // 斜角矩形
+        // BeveledRectangleBorder(
+        //   borderRadius: BorderRadius.circular(20),
         // ),
+        // 圆角矩形
+        // RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.circular(40),
+        // ),
+        // ),
+
         /// 自定义形状
         // shape: MyShape(),
         child: Row(
@@ -58,22 +134,6 @@ class _FloatActionBarPageState extends State<FloatActionBarPage> {
           ],
         ),
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: [
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: '首页',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.message_rounded),
-      //       label: '消息',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.people),
-      //       label: '我的',
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
@@ -83,7 +143,7 @@ class MyShape extends NotchedShape {
   @override
   Path getOuterPath(Rect host, Rect? guest) {
     var path = Path();
-    int wallCount = 6;
+    int wallCount = 10;
     double step = host.width / wallCount;
     double wall = host.height / 4;
     for (var i = 0; i < wallCount; i++) {
